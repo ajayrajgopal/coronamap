8b5e694cc11c7fb26cceebfd6cbd3acac2c82392
Ajay R <rajay2658@gmail.com>
2020-05-02 19:27:30 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styles from "./App.module.css";
+import Map from "./components/Map";
+import { FlyToInterpolator } from "react-map-gl";
+import { csv } from "d3";
+import GL from "@luma.gl/constants";
 
 function App() {
+  const [viewState, setViewState] = React.useState({
+    latitude: 20.5937,
+    longitude: 78.9629,
+    zoom: 4,
+    pitch: 45,
+    bearing: 10,
+  });
+
+  const heatMapData = csv("hexsample.csv", (d, id) => ({
+    id,
+    district: d["district"],
+    cases: d["cases"],
+    position: [+d["longitude"], +d["latitude"]],
+  })).then((patients) =>
+    patients.filter((d) => d.position[0] != null && d.position[1] != null)
+  );
+  const arcData = csv("arcsample.csv", (d, id) => ({
+    id,
+    patientId: d["p_id"],
+    position: [+d["longitude"], +d["latitude"]],
+    fromPos: [+d["fromLong"], +d["fromLat"]],
+  })).then((patients) =>
+    patients.filter((d) => d.position[0] != null && d.position[1] != null)
+  );
+  const handleChangeViewState = ({ viewState }) => setViewState(viewState);
+  const [patients, setPatients] = React.useState(heatMapData);
+  const [parameters, setParameters] = React.useState({});
+  const [type, setType] = React.useState("heatmap");
+
+  const switchTo3DHeatmap = () => {
+    if (type != "heatmap") {
+      setPatients(heatMapData);
+      setParameters({});
+      setType("heatmap");
+    }
+  };
+  const switchToArcs = () => {
+    if (type != "arc") {
+      setPatients(arcData);
+      setParameters({
+        blendFunc: [GL.SRC_ALPHA, GL.ONE, GL.ONE_MINUS_DST_ALPHA, GL.ONE],
+        blendEquation: GL.FUNC_ADD,
+      });
+      setType("arc");
+    }
+  };
+
+  const changeBearing = () => {
+    console.log("called");
+    var vb = 0.1;
+    var vp = 0.01;
+    if (Math.abs(viewState.bearing) > 89) {
+      vb *= -1;
+    }
+    setViewState({
+      ...viewState,
+      ...{ bearing: viewState.bearing + vb },
+    });
+    console.log(viewState.bearing);
+    window.requestAnimationFrame(changeBearing);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map
+        width="100vw"
+        height="100vh"
+        viewState={viewState}
+        onViewStateChange={handleChangeViewState}
+        patients={patients}
+        parameters={parameters}
+        type={type}
+      />
+      <div className={styles.controls}>
+        <button onClick={switchTo3DHeatmap}>3D Heatmap</button>
+        <button onClick={switchToArcs}>Arcs</button>
+        <button onMouseDown={changeBearing}>Left</button>
+      </div>
     </div>
   );
 }
---
+++ src/components/Map.js
@@ -0,0 +1,164 @@
+import React from "react";
+import ReactMapGL from "react-map-gl";
+import { DeckGL, HexagonLayer, ArcLayer, HeatmapLayer } from "deck.gl";
+import { AmbientLight, PointLight, LightingEffect } from "@deck.gl/core";
+import { Spring } from "react-spring/renderprops";
+
+export default function Map({
+  width,
+  height,
+  viewState,
+  patients,
+  onViewStateChange,
+  parameters,
+  type,
+}) {
+  var [elevate, setElevate] = React.useState(false);
+  var [tooltipData, setTooltipData] = React.useState({
+    text: "",
+    display: "none",
+    x: 0,
+    y: 0,
+  });
+  const ambientLight = new AmbientLight({
+    color: [255, 255, 255],
+    intensity: 1.0,
+  });
+
+  const pointLight1 = new PointLight({
+    color: [255, 255, 255],
+    intensity: 1.0,
+    position: [0, 0, 8000],
+  });
+
+  const pointLight2 = new PointLight({
+    color: [255, 255, 255],
+    intensity: 1.0,
+    position: [0, 0, 8000],
+  });
+
+  const lightingEffect = new LightingEffect({
+    ambientLight,
+    pointLight1,
+    pointLight2,
+  });
+
+  const material = {
+    ambient: 0.64,
+    diffuse: 0.6,
+    shininess: 32,
+    specularColor: [51, 51, 51],
+  };
+
+  function tooltip() {
+    return (
+      <div
+        style={{
+          position: "absolute",
+          top: tooltipData.y,
+          left: tooltipData.x,
+          color: "white",
+          display: tooltipData.display,
+          backgroundColor: "black",
+          fontSize: "12px",
+          padding: 5,
+          borderRadius: 5,
+          opacity: 0.8,
+        }}
+      >
+        District: {tooltipData.district}
+        <br />
+        Cases: {tooltipData.cases}
+      </div>
+    );
+  }
+  const toggleElevation = () => {
+    setElevate(true);
+  };
+  var layers = [];
+  var elevationScale = elevate ? 2500 : 0;
+  if (type == "heatmap") {
+    layers = [
+      new HexagonLayer({
+        id: "hexagon-layer",
+        data: patients,
+        extruded: true,
+        pickable: true,
+        elevationScale: elevationScale,
+        elevationRange: [0, 500],
+        autoHighlight: true,
+        radius: 6000,
+        transitions: {
+          elevationScale: 1000,
+        },
+        getPosition: (d) => {
+          return d.position;
+        },
+        getColorValue: (points) => {
+          return 3;
+        },
+        onHover: (info) => {
+          if (info.object !== null) {
+            setTooltipData({
+              district: info.object.points[0].district,
+              cases: parseInt(info.object.points[0].cases),
+              x: info.x,
+              y: info.y,
+              display: "block",
+            });
+          } else {
+            setTooltipData({ display: "none" });
+          }
+        },
+        getElevationWeight: (d) => {
+          return parseInt(d.cases);
+        },
+        coverage: 3,
+        upperPercentile: 100,
+        material: material,
+        colorRange: [
+          [1, 152, 189],
+          [73, 227, 206],
+          [216, 254, 181],
+          [254, 237, 177],
+          [254, 173, 84],
+          [209, 55, 78],
+        ],
+      }),
+    ];
+  }
+
+  if (type == "arc") {
+    layers = [
+      new ArcLayer({
+        id: "arc-layer",
+        data: patients,
+        getSourcePosition: (d) => d.position,
+        getTargetPosition: (d) => d.fromPos,
+        getSourceColor: [204, 51, 255],
+        getTargetColor: [148, 0, 211],
+        getWidth: 1.5,
+      }),
+    ];
+  }
+  return (
+    <ReactMapGL
+      width={width}
+      height={height}
+      viewState={viewState}
+      onViewStateChange={onViewStateChange}
+      onLoad={setElevate(true)}
+      mapStyle="mapbox://styles/mapbox/dark-v10"
+      mapboxApiAccessToken="pk.eyJ1IjoiYWpheXJhamdvcGFsIiwiYSI6ImNrMGphZzlkdjA1cDAzb3JheThvOHdqYTIifQ.kbO-Ok19Tu-0qg2FIVBt6Q"
+    >
+      <DeckGL
+        viewState={viewState}
+        effects={[lightingEffect]}
+        layers={layers}
+        parameters={parameters}
+      >
+        {tooltip}
+      </DeckGL>
+    </ReactMapGL>
+  );
+}
